9a208005e8347b00c6f4c4b21747bb6a7bc6847f
CriticalFlaw <nik_101@live.co.uk>
2020-08-11 22:53:51 -0400

    Added Python Version
    Added scripts from the python version of this project that's been scrapped.

---
+++ src/FlawBOT.Core/Extra/config.py
@@ -0,0 +1,9 @@
+HOST = "irc.twitch.tv"      # This is Twitchs IRC server
+PORT = 6667                 # Twitchs IRC server listens on port 6767
+NICK = "NULL"            	# Twitch username your using for your bot
+PASS = "NULL" 	            # your Twitch OAuth token
+CHAN = "NULL"               # the channel you want the bot to join.
+BAN_PAT = [
+    r"swear",
+    r"some_pattern"
+]
